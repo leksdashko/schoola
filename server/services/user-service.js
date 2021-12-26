a7840ce6708f8bc4f098087bc5b476f41e08e2e8
Alexander Dashko <leksdashko@gmail.com>
2021-12-26 22:33:46 +0200

    api registration finished

--- server/services/user-service.js
+++ server/services/user-service.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const mailService = require('./mail-service');
 const tokenService = require('./token-service');
+const UserDto = require('../dtos/user-dto');
 
 class UserService {
     async registration(email, password) {
@@ -14,7 +15,15 @@ class UserService {
         const activationLink = uuid.v4();
         const user = await UserModel.create({email, password: hashPassword});
         await mailService.sendActivationMail(email, activationLink);
-        const tokens = tokenService.generateTokens();
+
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({...userDto});
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return {
+            ...tokens,
+            user: userDto
+        }
     }
 }
 
