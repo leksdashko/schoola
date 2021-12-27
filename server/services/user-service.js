4c2dd72b0b167d00802a4c19f62836a6e4f2c573
Alexander Dashko <leksdashko@gmail.com>
2021-12-27 12:29:46 +0200

    google imap settings

--- server/services/user-service.js
+++ server/services/user-service.js
@@ -14,7 +14,7 @@ class UserService {
         const hashPassword = await bcrypt.hash(password, 3);
         const activationLink = uuid.v4();
         const user = await UserModel.create({email, password: hashPassword});
-        await mailService.sendActivationMail(email, activationLink);
+        await mailService.sendActivationMail(email, `${process.env.API_URL}/api/users/activate/${activationLink}`);
 
         const userDto = new UserDto(user);
         const tokens = tokenService.generateTokens({...userDto});
