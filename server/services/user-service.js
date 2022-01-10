2416e66389ad1e6a5742bc28962aafdfdd09361a
Alexander Dashko <leksdashko@gmail.com>
2022-01-10 23:56:22 +0200

    login

--- server/services/user-service.js
+++ server/services/user-service.js
@@ -35,6 +35,27 @@ class UserService {
         user.isActivated = true;
         await user.save();
     }
+
+    async login(email, password) {
+        const user = await UserModel.findOne({where: {email}});
+        if(!user){
+            throw ApiError.BadRequest('Користувач з такою email адресою не знайдений');
+        }
+        
+        const isPassEquals = await bcrypt.compare(password, user.password);
+        if(!isPassEquals) {
+            throw ApiError.BadRequest('Невірний email або пароль');
+        }
+
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({...userDto});
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return {
+            ...tokens,
+            user: userDto
+        }
+    }
 }
 
 module.exports = new UserService();
